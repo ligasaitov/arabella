17ca723ff699233ed6ee0712e5ad1f096f00c54b
ligasaitov <info@ligasaitov.ru>
2020-06-08 20:28:12 +0300

    contacts.html

--- arabella/js/main.js
+++ arabella/js/main.js
@@ -1103,4 +1103,128 @@ jQuery('img.svg').each(function () {
 	}, 'xml');
 
 });
+// function map(n){
+// 	google.maps.Map.prototype.setCenterWithOffset= function(latlng, offsetX, offsetY) {
+// 		var map = this;
+// 		var ov = new google.maps.OverlayView(); 
+// 		ov.onAdd = function() { 
+// 			var proj = this.getProjection(); 
+// 			var aPoint = proj.fromLatLngToContainerPixel(latlng);
+// 			aPoint.x = aPoint.x+offsetX;
+// 			aPoint.y = aPoint.y+offsetY;
+// 			map.panTo(proj.fromContainerPixelToLatLng(aPoint));
+// 			//map.setCenter(proj.fromContainerPixelToLatLng(aPoint));
+// 		}
+// 		ov.draw = function() {};
+// 		ov.setMap(this);
+// 	};
+// 	var markers = new Array();
+// 	var infowindow = new google.maps.InfoWindow({
+// 		//pixelOffset: new google.maps.Size(-230,250)
+// 	});
+// 	var locations = [
+// 		[new google.maps.LatLng(53.819055,27.8813694)],
+// 		[new google.maps.LatLng(53.700055,27.5513694)],
+// 		[new google.maps.LatLng(53.809055,27.5813694)],
+// 		[new google.maps.LatLng(53.859055,27.5013694)],
+// 	]
+// 	var options = {
+// 		zoom: 10,
+// 		panControl:false,
+// 		mapTypeControl:false,
+// 		center: locations[0][0],
+// 		styles:[{"featureType":"landscape.natural","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"color":"#e0efef"}]},{"featureType":"poi","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"hue":"#1900ff"},{"color":"#c0e8e8"}]},{"featureType":"road","elementType":"geometry","stylers":[{"lightness":100},{"visibility":"simplified"}]},{"featureType":"road","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"transit.line","elementType":"geometry","stylers":[{"visibility":"on"},{"lightness":700}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#7dcdcd"}]}],
+// 		scrollwheel:false,
+// 		mapTypeId: google.maps.MapTypeId.ROADMAP
+// 	}; 
+// 	var map = new google.maps.Map(document.getElementById('map'), options);
+// 	var icon={
+// 		url:'img/icons/map.svg',
+// 		scaledSize: new google.maps.Size(18, 20),
+// 		anchor: new google.maps.Point(9, 10)
+// 	}
+// 	for (var i = 0; i < locations.length; i++) {
+// 		var marker = new google.maps.Marker({
+// 			icon:icon,
+// 			position: locations[i][0],
+// 			map: map,
+// 		});
+// 		google.maps.event.addListener(marker, 'click', (function (marker, i) {
+// 			return function () {
+// 				for (var m = 0; m < markers.length; m++) {
+// 					markers[m].setIcon(icon);
+// 				}
+// 					var cnt=i+1;
+// 				infowindow.setContent($('.contacts-map-item_'+cnt).html());
+// 				infowindow.open(map, marker);
+// 				marker.setIcon(icon);
+// 				map.setCenterWithOffset(marker.getPosition(),0,0);
+// 				setTimeout(function(){
+// 					baloonstyle();
+// 				},10);
+// 			}
+// 		})(marker, i));
+// 		markers.push(marker);
+// 	}
+
+// 	if(n){
+// 			var nc=n-1;
+// 		setTimeout(function(){
+// 			google.maps.event.trigger(markers[nc], 'click');
+// 		},500);
+// 	}
+// }
+// function baloonstyle(){
+// 	$('.gm-style-iw').parent().addClass('baloon');
+// 	$('.gm-style-iw').prev().addClass('baloon-style');
+// 	$('.gm-style-iw').next().addClass('baloon-close');
+// 	$('.gm-style-iw').addClass('baloon-content');
+// }
+// if($("#map").length>0){
+// 	map(1);
+// }
+
+
+
+ymaps.ready(init);
+function init() {
+	// Создание карты.
+	var myMap = new ymaps.Map("map", {
+		// Координаты центра карты.
+		// Порядок по умолчанию: «широта, долгота».
+		// Чтобы не определять координаты центра карты вручную,
+		// воспользуйтесь инструментом Определение координат.
+		controls: [],
+		center: [55.73602656901537,37.662820499999945],
+		// Уровень масштабирования. Допустимые значения:
+		// от 0 (весь мир) до 19.
+		zoom: 15
+	});
+
+	myPlacemark = new ymaps.Placemark([55.73602656901537,37.662820499999945],{
+		id:'2'
+	},
+	{
+	// Опции.
+	hasBalloon:false,
+	hideIconOnBalloonOpen:false,
+	// Необходимо указать данный тип макета.
+	iconLayout: 'default#imageWithContent',
+	// Своё изображение иконки метки.
+	iconImageHref: 'img/icon/point.png',
+	// Размеры метки.
+	iconImageSize: [90, 100],
+	// Смещение левого верхнего угла иконки относительно
+	// её "ножки" (точки привязки).
+	iconImageOffset: [-40, -40],
+	// Смещение слоя с содержимым относительно слоя с картинкой.
+	iconContentOffset: [0,0],
+});
+	myMap.geoObjects.add(myPlacemark);
+
+	myMap.behaviors.disable('scrollZoom');
+
+	// myMap.behaviors.disable('scrollZoom');
+}
+
 });
