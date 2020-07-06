93ffa6974568fd23a40b47c251d5c81a0b97183f
ligasaitov <info@ligasaitov.ru>
2020-07-06 11:05:38 +0300

    + - input

--- arabella/js/main.js
+++ arabella/js/main.js
@@ -1102,7 +1102,20 @@ catNavIcon.click(function(event) {
 	$(this).parent().toggleClass('active');
 });
 
-
+$('.goods__card_quantity_minus').click(function () {
+	var $input = $(this).parent().find('input');
+	var count = parseInt($input.val()) - 1;
+	count = count < 1 ? 1 : count;
+	$input.val(count);
+	$input.change();
+	return false;
+});
+$('.goods__card_quantity_plus').click(function () {
+	var $input = $(this).parent().find('input');
+	$input.val(parseInt($input.val()) + 1);
+	$input.change();
+	return false;
+});
 // MAIN SLIDER
 let mainSlider = $('.main__slider');
 if(mainSlider.length > 0) {
